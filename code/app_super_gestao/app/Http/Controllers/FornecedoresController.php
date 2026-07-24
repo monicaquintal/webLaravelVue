2ba2969ec7cd831789716f669ad24ab6d1ecd754
Monica Quintal <monica.zoom@hotmail.com>
2026-07-24 09:19:03 -0300

    dia 4

--- code/app_super_gestao/app/Http/Controllers/FornecedoresController.php
+++ code/app_super_gestao/app/Http/Controllers/FornecedoresController.php
@@ -8,18 +8,14 @@ class FornecedoresController extends Controller
 {
     public function index() {
         $fornecedores = [
-            'Fornecedor1',
-            'Fornecedor1',
-            'Fornecedor1',
-            'Fornecedor1',
-            'Fornecedor1',
-            'Fornecedor1',
-            'Fornecedor1',
-            'Fornecedor1',
-            'Fornecedor1',
-            'Fornecedor1',
-            'Fornecedor1'
+            0 => [
+                'nome' => 'Fornecedor1', 
+                'status' => 'N', 
+                'cnpj' => '00.000.000/0000-00'],
+            1 => [
+                'nome' => 'Fornecedor2', 
+                'status' => 'S']   
             ];
-        return view('app.fornecedor.index', compact('fornecedores'));
+        return view('app.fornecedor.index', compact('fornecedores'));;
     }
 }
--- code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ code/app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,22 +1,18 @@
 <h3>Fornecedor</h3>
 
-
 @php
-
-/* if() {
-
-} else if {
-
-} else {
-
-} */
-
+    /*
+    if (isset($variavel)) {}
+        retorna true se a variável estiver definida
+    */
 @endphp
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados.</h3>
-@elseif(count($fornecedores) >= 10)
-    <h3>Existem vários fornecedores cadastrados.</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados.</h3>
-@endif
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[1]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[1]['status'] }}
+    <br>
+    @isset($fornecedores[1]['cnpj'])
+        CNPJ: {{ $fornecedores[1]['cnpj'] }}
+    @endisset
+@endisset
