3a8c2f7a65048e9cf21d8979912d3834e708203a
Monica Quintal <monica.zoom@hotmail.com>
2026-08-03 17:06:51 -0300

    dia 10

--- code/app_super_gestao/app/Http/Controllers/ContatoController.php
+++ code/app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
-        echo '<pre>';
-        print_r($request->all());
-        echo '</pre>';
-        echo $request->input('nome');
-        echo '<br>';
-        echo $request->input('email');
+
+        // $contato = new SiteContato();
+        // $contato->create($request->all());
+
         return view('site.contato', ['titulo' => 'Contato (teste)']);
+    }
+
+    public function salvar(Request $request) {
+        
+        
+
+        //SiteContato::create($request->all());
+
     }
 }
