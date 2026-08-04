9176530a38f6e364b581075a1702cf6343f3c6d0
Monica Quintal <monica.zoom@hotmail.com>
2026-08-04 10:34:14 -0300

    dia 11

--- code/app_super_gestao/app/Http/Controllers/ContatoController.php
+++ code/app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -17,7 +17,13 @@ class ContatoController extends Controller
 
     public function salvar(Request $request) {
         
-        
+        $request->validate([
+            'nome' => 'required | min:3 | max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required | max:2000'
+        ]);
 
         //SiteContato::create($request->all());
 
