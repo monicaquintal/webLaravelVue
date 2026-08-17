f9916905b154ecf6af3d7478dd3fd62a18532118
Monica Quintal <monica.zoom@hotmail.com>
2026-08-17 16:36:34 -0300

    dia 19

--- code/app_super_gestao/app/Http/Controllers/ProdutoDetalheController.php
+++ code/app_super_gestao/app/Http/Controllers/ProdutoDetalheController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Unidade;
 use App\Models\ProdutoDetalhe;
+use App\Models\ItemDetalhe;
 
 class ProdutoDetalheController extends Controller
 {
@@ -31,8 +32,9 @@ class ProdutoDetalheController extends Controller
     }
 
  
-    public function edit(ProdutoDetalhe $produtoDetalhe)
+    public function edit($id)
     {
+        $produtoDetalhe = ItemDetalhe::find($id);
         $unidades = Unidade::all();
         return view('app.produto_detalhe.edit', ['produto_detalhe' => $produtoDetalhe, 'unidades' => $unidades]);
     }
---
+++ code/app_super_gestao/app/Models/Item.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    protected $table = 'produtos';
+    protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id'];
+
+    public function itemDetalhe() {
+        return $this->hasOne('App\Models\ItemDetalhe', 'produto_id', 'id');
+    }
+}
