d1959b5cb0c72f2b07d1af68e0ce7a88be25cf1b
Monica Quintal <monica.zoom@hotmail.com>
2026-08-27 10:26:22 -0300

    dia 27

--- code/app_controle_tarefas/routes/web.php
+++ code/app_controle_tarefas/routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 use App\Http\Controllers\TarefaController;
+use App\Mail\MensagemTesteMail;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +15,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('tarefa', TarefaController::class);
+
+Route::get('/mensagem-teste', function() {
+    return new MensagemTesteMail();
+    // Mail::to('monica.zoom@hotmail.com')->send(new MensagemTesteMail());
+    // return 'E-mail enviado com sucesso!';
+});
